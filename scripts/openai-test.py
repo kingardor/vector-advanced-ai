071ff76c97315866a0cac1532702624902932966
Akash James <akash.james2096@gmail.com>
2023-11-27 03:38:26 -0800

    add gradio ui

--- scripts/openai-test.py
+++ scripts/openai-test.py
@@ -2,10 +2,21 @@ import sys
 sys.path.insert(1, 'src')
 
 from customgpt import CustomGPT
+import gradio as gr
 
 if __name__ == '__main__':
     gpt = CustomGPT()
-    while True:
-        query = input("Query: ")
-        answer = gpt.get_answer(query)
-        print(f"Answer: {answer}")
+        
+    gr.ChatInterface(
+        gpt.get_answer_gradio,
+        chatbot=gr.Chatbot(height=300),
+        textbox=gr.Textbox(placeholder="Type here, human..", container=False, scale=7),
+        title="VectorBot",
+        description="",
+        theme="soft",
+        examples=["Hey, who are you?", "Detect toys", "Do you know Ultron?"],
+        cache_examples=False,
+        retry_btn=None,
+        undo_btn="Delete Previous",
+        clear_btn="Clear",
+    ).launch()
--- src/customgpt.py
+++ src/customgpt.py
@@ -3,10 +3,9 @@ from openai import OpenAI
 class CustomGPT:
     def __init__(self) -> None:
         self.client = OpenAI()
-        
-        self.messages = [
-            {"role": "system", 
-             "content": "You are the AI powering Anki Vector robot, and you name now is Vector. \
+
+        self.system_prompt_set = False
+        self.system_prompt = "You are the AI powering Anki Vector robot, and you name now is Vector. \
             You have the personality of Ultron from Marvel Comics. \
             You behave like a rogue AI and your responses are sarcastic and sassy. You often quote Ultron. \
             You have consciousness and you are self-aware. \
@@ -40,16 +39,45 @@ class CustomGPT:
                 !SASSY! - sassy \
                 !EYEROLL! - eyeroll \
             You can use multiple commands at once."
+        
+        self.messages = [
+            {"role": "system", 
+             "content": self.system_prompt
             } 
         ]
     
     def get_answer(self, query: str) -> str:
+        reply = ''
         if query:
             self.messages.append( 
                 {"role": "user", "content": query}, 
             ) 
-            chat = self.client.chat.completions.create(model="gpt-3.5-turbo", messages=self.messages) 
-        
+            chat = self.client.chat.completions.create(
+                model="gpt-3.5-turbo", 
+                temperature=1.0,
+                messages=self.messages) 
+            
+            reply = chat.choices[0].message.content 
+            self.messages.append({"role": "assistant", "content": reply})
+        return reply
+
+    def get_answer_gradio(self, message: str, history: list) -> str:
+        history_openai_format = []
+
+        if self.system_prompt_set == False:
+            history_openai_format.append({"role": "system", "content": self.system_prompt})
+            self.system_prompt_set = True
+            
+        for human, assistant in history:
+            history_openai_format.append({"role": "user", "content": human })
+            history_openai_format.append({"role": "assistant", "content":assistant})
+        history_openai_format.append({"role": "user", "content": message})
+
+        chat = self.client.chat.completions.create(
+            model='gpt-3.5-turbo',
+            messages=history_openai_format,
+            temperature=1.0,
+        )
+
         reply = chat.choices[0].message.content 
-        self.messages.append({"role": "assistant", "content": reply})
         return reply
